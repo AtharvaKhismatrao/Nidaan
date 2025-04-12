0783c729f58a73a111015a3f261054d7b762028d
Atharva Khismatrao <khismatraoatharva2005@gmail.com>
2025-04-13 01:25:27 +0530

    Delete Nidaan directory

--- Nidaan/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // ✅ Set base to '/' for Vercel (or './' if you ever host in a subdirectory)
-})
